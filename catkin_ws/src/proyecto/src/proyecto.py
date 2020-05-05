3676467ac91ef8f789a4f9f3e9cf81f98439639b
Sebas Reyes <sebastianseyer@hotmail.com>
2020-05-05 16:49:46 -0500

    Proyecto Terminado

--- catkin_ws/src/proyecto/src/proyecto.py
+++ catkin_ws/src/proyecto/src/proyecto.py
@@ -8,58 +8,54 @@ from move_base_msgs.msg import MoveBaseActionResult
 from geometry_msgs.msg import PoseStamped
 
 place = "goal"
+pub_goal= rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=10)
+pub_speech = rospy.Publisher("robotsound", SoundRequest, queue_size=10)
+msg_speech = SoundRequest()
+msg_speech.sound   = -3
+msg_speech.command = 1
+msg_speech.volume  = 1.0
+msg_speech.arg2    = "voice_us3_mbrola"
 
 def callback_function(msg):
 	global place 
-	pub_goal= rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=10)
+	global pub_goal
 	loop = rospy.Rate(2)
 	goal=PoseStamped()
 	goal.pose.orientation.w=0.5
 	goal.header.frame_id="map"
 	x = msg.data.split(" ")
-	place = x[4]
+	place = x[2]
 	print('goal: {}'.format(place))
 	if(place=='EXIT'):
-		goal.pose.position.x=9.57
-		goal.pose.position.y=0.07
-		print(goal)
-		pub_goal.publish(goal)
+		goal.pose.position.x=8.10
+		goal.pose.position.y=0.86
 	elif(place=='ENTRY'):
-		goal.pose.position.x=0.0
+		goal.pose.position.x=2.0
 		goal.pose.position.y=0.0
-		print(goal)
-		pub_goal.publish(goal)
 	elif(place=='CENTER'):
 		goal.pose.position.x=5.04
-		goal.pose.position.y=3.45
-		print(goal)
-		pub_goal.publish(goal)
-	elif(place=='CORNER'):
-		goal.pose.position.x=9.40
-		goal.pose.position.y=7.03
-		print(goal)
-		pub_goal.publish(goal)
+		goal.pose.position.y=2.00
+	elif(place=='END'):
+		goal.pose.position.x=8.86
+		goal.pose.position.y=6.42
 	elif(place=='NOOK'):
 		goal.pose.position.x=1.20
 		goal.pose.position.y=6.86
-		print(goal)
-		pub_goal.publish(goal)
 	else:
 		loop.sleep()
-
+	pub_goal.publish(goal)
 
 def callback(msg):
-	pub_speech = rospy.Publisher("robotsound", SoundRequest, queue_size=10)
-	loop = rospy.Rate(2)
-	msg_speech = SoundRequest()
-	msg_speech.sound   = -3
-	msg_speech.command = 1
-	msg_speech.volume  = 1.0
-	msg_speech.arg2    = "voice_us3_mbrola"
-	msg_speech.arg = "the robot has arrive to the " + place
-	loop.sleep()
-	pub_speech.publish(msg_speech)
-	print('{}'.format(msg.status.text))
+	global pub_speech
+	if(msg.status.text=="Goal reached."):
+		print("{} The robot has arrived to the {}".format(msg.status.text,place))
+		loop = rospy.Rate(2)
+		msg_speech.arg = "I have arrived to the " + place
+		loop.sleep()
+		pub_speech.publish(msg_speech)
+	else:
+		print(msg.status.text)
+
 
 def main():
 	print "INITIALIZING SPEECH SYNTHESIS NODE..."
--- catkin_ws/src/proyecto/src/prueba.py
+++ catkin_ws/src/proyecto/src/prueba.py
@@ -1,15 +1,72 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
+from sound_play.msg import SoundRequest
+from std_msgs.msg import String
 from move_base_msgs.msg import MoveBaseActionResult
+from geometry_msgs.msg import PoseStamped
+
+place = "goal"
+pub_goal= rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=10)
+pub_speech = rospy.Publisher("robotsound", SoundRequest, queue_size=10)
+msg_speech = SoundRequest()
+msg_speech.sound   = -3
+msg_speech.command = 1
+msg_speech.volume  = 1.0
+msg_speech.arg2    = "voice_us3_mbrola"
+
+def callback_function(msg):
+	global place 
+	global pub_goal
+	loop = rospy.Rate(2)
+	goal=PoseStamped()
+	goal.pose.orientation.w=0.5
+	goal.header.frame_id="map"
+	x = msg.data.split(" ")
+	place = x[2]
+	print('goal: {}'.format(place))
+	if(place=='EXIT'):
+		goal.pose.position.x=7.57
+		goal.pose.position.y=0.07
+	elif(place=='ENTRY'):
+		goal.pose.position.x=2.0
+		goal.pose.position.y=0.0
+	elif(place=='CENTER'):
+		goal.pose.position.x=5.04
+		goal.pose.position.y=3.45
+	elif(place=='END'):
+		goal.pose.position.x=8.86
+		goal.pose.position.y=6.42
+	elif(place=='NOOK'):
+		goal.pose.position.x=1.20
+		goal.pose.position.y=6.86
+	else:
+		loop.sleep()
+	pub_goal.publish(goal)
 
 def callback(msg):
-	print('{}'.format(msg.status.text))
+	global pub_speech
+	if(msg.status.text=="Goal reached."):
+		print("{} The robot has arrived to the {}".format(msg.status.text,place))
+		loop = rospy.Rate(2)
+		msg_speech.arg = "I have arrived to the " + place
+		loop.sleep()
+		pub_speech.publish(msg_speech)
+	else:
+		print(msg.status.text)
+
 
 def main():
-	rospy.init_node('prueba')
+	print "INITIALIZING SPEECH SYNTHESIS NODE..."
+	rospy.init_node("speech_synthesize")
+	rospy.Subscriber("/recognized",String, callback_function)
 	rospy.Subscriber("/move_base/result" , MoveBaseActionResult , callback)
 	rospy.spin()
 
+
 if __name__ == '__main__':
-	main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
