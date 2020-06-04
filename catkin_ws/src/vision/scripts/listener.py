29c9bfef0110fa0969b6c0fdca9d304dd2e50bd3
Mark The Magnificent <contact@mnegretev.info>
2020-06-03 21:41:14 -0500

    Calificación - Proyecto: 10

---
+++ catkin_ws/src/vision/scripts/listener.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from move_base_msgs.msg import MoveBaseActionResult
+from sound_play.msg import SoundRequest
+
+def callback(data):
+  pub_speech = rospy.Publisher("robotsound", SoundRequest, queue_size=10)
+
+  loop = rospy.Rate(2)
+
+  msg_speech = SoundRequest()
+  msg_speech.sound   = -3
+  msg_speech.command = 1
+  msg_speech.volume  = 1.0
+  msg_speech.arg2    = "voice_kal_diphone"
+
+  if data.status.status == 3:
+    msg_speech.arg = "Goal reached"
+    print("Goal reached")
+  elif data.status.status == 4:
+    msg_speech.arg = "I can't do it"
+    print("An error has ocurred")
+  loop.sleep()
+  print "Sending text to say: " + msg_speech.arg
+  pub_speech.publish(msg_speech)
+ 
+def listener():
+  # In ROS, nodes are uniquely named. If two nodes with the same
+  # name are launched, the previous one is kicked off. The
+  # anonymous=True flag means that rospy will choose a unique
+  # name for our 'listener' node so that multiple listeners can
+  # run simultaneously.
+  rospy.init_node('listener', anonymous=True)
+
+  rospy.Subscriber("move_base/result", MoveBaseActionResult, callback)
+
+  # spin() simply keeps python from exiting until this node is stopped
+  rospy.spin()
+
+if __name__ == '__main__':
+  listener()
